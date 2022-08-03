b8b50211a0073daf5a615f347ac58caa2c4efbec
aep <aepsaprul18@gmail.com>
2022-08-03 11:16:51 +0700

    responsive customer page

--- resources/views/pages/situmpur/customer.blade.php
+++ resources/views/pages/situmpur/customer.blade.php
@@ -13,7 +13,7 @@
     <section class="content">
         <div class="container-fluid">
             <div class="d-flex justify-content-center">
-                <div class="col-4">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
 
                     {{-- flash message --}}
                     @if ($message = Session::get('error'))
